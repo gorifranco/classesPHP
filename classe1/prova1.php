c26820a3b96f2ee7e77b87a016664631eb730981
Gori Franco <gorifranco@gmail.com>
2023-10-05 20:59:54 +0200

    05/10/2023

--- classe1/prova1.php
+++ classe1/prova1.php
@@ -1,55 +1,21 @@
 <?php
+$CIUTATS = array("Alcúdia"=>array("Habitants"=>19172, "Illa"=>"Mallorca"), "Formentera"=>array("Habitants"=>9962, "Illa"=>"Formentera"),
+    "Alaior"=>array("Habitants"=>9399, "Illa"=>"Menorca"), "Ciutadella"=>array("Habitants"=>29247, "Illa"=>"Menorca"));
 
-$alumnes= array ("1122"=>
-
-    array("nom"=>"Pep",
-
-        "telefons"=>array("971223344"," 666778877"),
-
-        "grup"=>"S1X",
-
-        "dir"=>array("carrer"=>"Nou","Num"=>12,"CP"=>"07300","Poblacio"=>"Inca"),
-
-        "notes"=>array("client"=>7,"seridor"=>8,"disseny"=>5,"desplegament"=>7)
-
-    ),
-
-    "1192"=>
-
-        array("nom"=>"Joan",
-
-            "telefons"=>array("699775544"),
-
-            "grup"=>"S1G",
-
-            "dir"=>array("carrer"=>"Major","Num"=>1,"CP"=>"07440","Poblacio"=>"Muro"),
-
-            "notes"=>array("client"=>6,"seridor"=>5,"disseny"=>5)
-
-        ),
-
-    "1982"=>
-
-        array("nom"=>"Miquel",
-
-            "telefons"=>array("971889977","666000077"),
-
-            "grup"=>"S1X",
-
-            "dir"=>array("carrer"=>"Sa plasa","Num"=>6,"CP"=>"07300","Poblacio"=>"Inca"),
-
-            "notes"=>array("client"=>9,"seridor"=>10,"disseny"=>8,"desplegament"=>6)
-
-        )
-
-);
-
-echo "<br>".$alumnes["1122"]["grup"];
-echo "<br> telefons: ";
-foreach ($alumnes["1982"]["telefons"] as $tf){
-    echo "<br>".$tf;
+echo "Exercici a)<br>";
+ksort($CIUTATS);
+$total = 0;
+echo "<table>";
+foreach ($CIUTATS as $item => $cc){
+    $total += $cc["Habitants"];
+    echo "<tr><td>$item</td><td>".$cc["Habitants"]."</td><td>".$cc["Illa"]."</td></tr>";
 }
-echo "<br>";
-foreach ($alumnes as $i =>$al){
-    echo "<br>Codi: ".$i." Nom: ".$al["nom"]." Grup: ".$al["grup"];
+echo "<tr><td>Total</td><td>$total</td></tr>";
+echo "</table>";
+
+echo "<br>Exercici b)<br>";
+asort($CIUTATS);
+foreach ($CIUTATS as $item => $cc){
+    echo "<tr><td>$item</td><td>".$cc["Habitants"]."</td><td>".$cc["Illa"]."</td></tr>";
 }
+?>
