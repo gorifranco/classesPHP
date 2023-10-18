a8c31def95156f0258ece88a2b8c0c6cbc437c82
Espechan1 <esperanzaperez@paucasesnovescifp.cat>
2023-10-19 00:19:11 +0200

    casi res

---
+++ funcions/exercici1.php
@@ -0,0 +1,5 @@
+<?php
+function rectangle($altura, $amplada){
+    return "Un rectangle amb una altura de $altura i una amplada de $amplada té una àrea de " .($altura * $amplada). ". I un perímetre
+de " . (2*$altura+2*$amplada) . ".";
+}
