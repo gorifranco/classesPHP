89e2de6bb90431e5ba04887767e0e6e42c4a66ef
gori <gorifranco@gmail.com>
2023-10-26 00:46:05 +0200

    carrito acabat

---
+++ botiga.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+<div class="container">
+    <br/>
+    <div class="row">
+        <h1 class="col-md-6 mx-auto display-4">Tabla amb Bootstrap</h1>
+    </div>
+    <br/>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "1234";
+    $dbname = "la_meva_botiga";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname, 3307);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT productes.id as id, productes.nom as nomProducte, productes.descripció as descripcio, preu, categories.nom as nomCategoria FROM productes
+    inner join categories on categories.id = productes.categoria_id;";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table class='table table-striped'>";
+        echo '<tr class="table-secondary"><td>Id</td><td>Nom del producte</td><td>Descripció</td><td>Preu</td><td>Categoria</td></tr>';
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr><td class="table-secondary">' . $row["id"] . '</td><td>' . $row["nomProducte"] . '
+                </td><td>' . $row["descripcio"] . '</td><td>' . $row["preu"] . '</td><td>' . $row["nomCategoria"] . '</td></tr>';
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
+</div>
+</body>
+</html>
--- sessions/carrito/comprova.php
+++ sessions/carrito/comprova.php
@@ -1,44 +1,59 @@
 <?php
 session_start();
-if (isset($_SESSION["mail"])) {
-    echo "Uep, com va " . $_SESSION["mail"] . "?<br/>";
-    echo "<a href='logout.php'>logout</a><br/>";
-} else {
-    header("Location: login.php");
-}
-
-$productes = ["0000" => ["cireres", "gut shit", 5], "0001" => ["llom", "llom de trutja", 2], "0002" => ["colquers", "venenplens", 7],
-    "0003" => ["curry", "Lo més bo de sa llista", 1], "0004" => ["pomes", "d'un pomer", 1.5]];
-
-if(isset($_POST["submit"])){
-    $_SESSION["carrito"] = [];
-    foreach ($productes as $key => $producte){
-        if($_POST[$key] > 0){
-            array_push([$producte[0], ]);
-            $_SESSION["carrito"]
-        }
-    }
-}
-
 ?>
+
 <html>
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-<form method="post" action="#">
+<div class="container">
     <?php
-    echo '<ul>';
-    foreach ($productes as $key => $producte) {
-        echo '<li><label>' . ucfirst($producte[0]) . ':';
-        echo '<input type="number" value="0" min="0" name="' . $key . '">';
-        echo 'Descripció: ' . $producte[1] . ' ';
-        echo 'Preu: ' . $producte[2];
-        echo "</label></li>";
+    if (isset($_SESSION["mail"])) {
+        echo "Uep, com va " . $_SESSION["mail"] . "?<br/>";
+        echo "<a href='logout.php'>logout</a><br/>";
+    } else {
+        header("Location: login.php");
     }
-    echo '</ul>';
+
+    $productes = ["0000" => ["cireres", "gut shit", 5], "0001" => ["llom", "llom de trutja", 2], "0002" => ["colquers", "venenplens", 7],
+        "0003" => ["curry", "Lo més bo de sa llista", 1], "0004" => ["pomes", "d'un pomer", 1.5]];
+
+    if(isset($_POST["submit"])){
+        $_SESSION["carrito"] = [];
+        foreach ($productes as $key => $producte){
+            if($_POST[$key] > 0){
+                $_SESSION["carrito"][] = [$key, $_POST[$key]];
+            }
+        }
+    }
+    function treureValue($array, $key){
+        foreach ($array as $value){
+            if($value[0] == $key) return $value[1];
+        }
+        return 0;
+    }
     ?>
-<input type="submit" value="Enviar" name="submit">
-</form>
+</div>
+
+<div class="container">
+    <form method="post" action="#">
+        <?php
+        echo '<ul>';
+        foreach ($productes as $key => $producte) {
+            echo '<li><label>' . ucfirst($producte[0]) . ':';
+
+            echo '<input type="number" value="'.treureValue($_SESSION["carrito"],$key).'" min="0" name="' . $key . '">';
+
+            echo ' Descripció: ' . $producte[1] . ' ';
+            echo ' Preu: ' . $producte[2];
+            echo "</label></li>";
+        }
+        echo '</ul>';
+        ?>
+        <input type="submit" value="Enviar" name="submit">
+    </form>
+</div>
 </body>
 </html>
 
