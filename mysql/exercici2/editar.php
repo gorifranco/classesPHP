b2f90bf7ed8219444f1f58165e84ac0a0b3c8fd7
Gori Franco <gorifranco@gmail.com>
2023-11-15 18:59:57 +0100

    acabant editar

--- mysql/exercici2/editar.php
+++ mysql/exercici2/editar.php
@@ -1,2 +1,153 @@
 <?php
 session_start();
+
+dadesLlibre();
+
+if(isset($_POST["submit"])){
+    $connexio = new Mysqli();
+    $connexio->connect("localhost", "root", "1234", "biblioteca", 3306);
+    $sql1 = "select id_edit from editors where nom_edit = {$_POST["editorial"]}";
+    $nom_editor = (($connexio->query($sql1))->fetch_assoc())["id_edit"];
+
+    $sql = "update llibres set titol=?, fkidedit=?, fk_departament=?, llocedicio=? where id_llib = {$_GET["id"]}";
+    $prepared = $connexio->prepare($sql);
+
+    if (!$prepared) {
+        die("Error en la preparación de la consulta: " . $connexio->error);
+    }
+
+    $prepared->bind_param("siss", $_POST["titol"], $nom_editor, $_POST["departament"], $_POST["llocedicio"]);
+
+    if ($prepared->execute() === TRUE) {
+        echo "Tupla actualizada correctamente";
+    } else {
+        echo "Error al actualizar la tupla: " . $connexio->error;
+    }
+}
+
+function dadesLlibre(){
+    $connexio = new Mysqli();
+    $connexio->connect("localhost", "root", "1234", "biblioteca", 3306);
+
+    $sql = "select id_llib as id, titol, nom_edit, fk_departament, llocedicio from llibres
+        inner join editors on editors.id_edit = llibres.fk_idedit where id_llib = {$_GET["id"]}";
+
+    $prepared = $connexio->prepare($sql);
+
+    $prepared->execute();
+    $_SESSION["dades"] = ($prepared->get_result())->fetch_assoc();
+    $prepared->close();
+    $connexio->close();
+}
+
+function optionsDepartament(){
+    $connexio = new Mysqli();
+    $connexio->connect("localhost", "root", "1234", "biblioteca", 3306);
+
+    $sql = "select departament from departaments";
+
+    $prepared = $connexio->prepare($sql);
+
+    $prepared->execute();
+    $result_set = $prepared->get_result();
+
+    $array = array();
+
+    while ($row = $result_set->fetch_assoc()) {
+        $array[] = $row;
+    }
+
+    $prepared->close();
+    $result_set->close();
+    $connexio->close();
+
+    foreach ($array  as $dep){
+        if($dep["departament"] == $_SESSION["dades"]["fk_departament"]){
+        echo "<option selected='selected'>{$dep["departament"]}</option>";
+        }else{
+            echo "<option>{$dep["departament"]}</option>";
+        }
+    }
+}
+
+function optionsEditorial(){
+
+    $connexio = new Mysqli();
+    $connexio->connect("localhost", "root", "1234", "biblioteca", 3306);
+
+    $sql = "select nom_edit as editor from editors";
+
+    $prepared = $connexio->prepare($sql);
+
+    $prepared->execute();
+    $result_set = $prepared->get_result();
+
+    $array = array();
+
+    while ($row = $result_set->fetch_assoc()) {
+        $array[] = $row;
+    }
+
+    $prepared->close();
+    $result_set->close();
+    $connexio->close();
+
+    foreach ($array  as $edit){
+        if($edit["editor"] == $_SESSION["dades"]["nom_edit"]){
+            echo "<option selected='selected'>{$edit["editor"]}</option>";
+        }else{
+            echo "<option>{$edit["editor"]}</option>";
+        }
+    }
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <title>Editar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container mt-5 border p-4">
+    <form action="editar.php?id=<?=$_GET["id"]?>" method="post">
+        <div class="text-center"><h2>Llibres</h2></div>
+    <div class="row form-row">
+        <div class="form-group col-md-4">
+            <label for="id_llib">ID</label>
+            <input type="number" class="form-control" id="id_llib" disabled="disabled" value="<?=$_SESSION["dades"]["id"]?>">
+        </div>
+        <div class="form-group col-md-8">
+            <label for="titol">Títol</label>
+            <input name="titol" type="text" class="form-control" id="titol" placeholder="titol" value="<?=$_SESSION["dades"]["titol"]?>">
+        </div>
+    </div>
+    <div class="row form-row">
+        <div class="form-group col-md-4">
+            <label for="lloc_edicio">Lloc Edició</label>
+            <input name="llocedicio" type="text" class="form-control" id="lloc_edicio" placeholder="Lloc edició" value="<?=$_SESSION["dades"]["llocedicio"]?>">
+        </div>
+        <div class="form-group col-md-4">
+            <label for="departament">Departament</label>
+            <select name="departament" id="departament" class="form-control">
+                <?php optionsDepartament();?>
+            </select>
+        </div>
+        <div class="form-group col-md-4">
+            <label for="editorial">Editorial</label>
+            <select name="editorial" id="editorial" class="form-control">
+                <?php optionsEditorial();?>
+            </select>
+        </div>
+    </div>
+<div class="text-center">
+    <button type="submit" class="btn btn-primary mt-3" name="submit">Guardar</button>
+    <button type="button" class="btn btn-danger mt-3" name="delete">Eliminar</button>
+</div>
+</form>
+</div>
+</body>
+</html>
--- mysql/exercici2/index.php
+++ mysql/exercici2/index.php
@@ -5,36 +5,60 @@ session_start();
 $_SESSION["selected"] = (isset($_GET["sentit"]))?$_GET["ordre"].$_GET["sentit"]:"idasc";
 $_SESSION["sentit"] = (isset($_GET["sentit"]))?$_GET["sentit"]:"asc";
 $_SESSION["ordre"] = (isset($_GET["ordre"]))?$_GET["ordre"]:"id";
+$_SESSION["limit"] = 10;
+if(!isset($_SESSION["offset"])) $_SESSION["offset"] = 0;
+if(isset($_GET["pg"])){
+    if($_GET["pg"] == "darrera"){
+        $_SESSION["offset"] = floor($_SESSION["totalTuples"]/$_SESSION["limit"])*$_SESSION["limit"];
+    }else {
+        $_SESSION["offset"] = ($_GET["pg"] - 1) * $_SESSION["limit"];
+    }
+}
+
 
 if(isset($_GET["ordre"]) && $_GET["ordre"] == "editor") $_SESSION["ordre"] = "nom_edit";
 if(isset($_SESSION["filtre"])){
     if(isset($_GET["filtre"])){
         $_SESSION["filtre"] = $_GET["filtre"];
+        $_SESSION["offset"] = 0;
     }
 }else{
     $_SESSION["filtre"] = "";
 }
 
 $_SESSION["arrayFiles"] = treureLlibres();
+totalTuples();
 
+function totalTuples(){
+    $connexio = new Mysqli();
+    $connexio->connect("localhost", "root", "1234", "biblioteca", 3306);
+
+    $sql = "select count(*) as total from llibres
+        right join editors on editors.id_edit = llibres.fk_idedit
+        where id_llib like '%{$_SESSION["filtre"]}%' or titol like '%{$_SESSION["filtre"]}%' or nom_edit like '%{$_SESSION["filtre"]}%'";
+    $prepared = $connexio->prepare($sql);
+
+    $prepared->execute();
+    $result_set = $prepared->get_result();
+    $_SESSION["totalTuples"] = ($result_set->fetch_assoc())["total"];
+    $prepared->close();
+    $connexio->close();
+
+}
 function treureLlibres()
 {
     $connexio = new Mysqli();
     $connexio->connect("localhost", "root", "1234", "biblioteca", 3306);
 
     $sql = "select id_llib as id, titol, nom_edit from llibres
-        inner join editors on editors.id_edit = llibres.fk_idedit
-        where id like %'{$_SESSION["filtre"]}'% or titol like %'{$_SESSION["filtre"]}'% or nom_edit like %'{$_SESSION["filtre"]}'% 
-        order by {$_SESSION["ordre"]} {$_SESSION["sentit"]} limit 10 offset 0";
-
-
+        right join editors on editors.id_edit = llibres.fk_idedit
+        where id_llib like '%{$_SESSION["filtre"]}%' or titol like '%{$_SESSION["filtre"]}%' or nom_edit like '%{$_SESSION["filtre"]}%' 
+        order by {$_SESSION["ordre"]} {$_SESSION["sentit"]} limit {$_SESSION["limit"]} offset {$_SESSION["offset"]}";
 
     $prepared = $connexio->prepare($sql);
 
-
     $prepared->execute();
     $result_set = $prepared->get_result();
-
 
     $array = array();
 
@@ -55,6 +79,23 @@ function imprimirFilesTabla($arrayFiles)
     }
 }
 
+function imprimirPaginacio()
+{
+    if ($_SESSION["offset"] == 0 || $_SESSION["offset"] == $_SESSION["limit"]) {
+        $valors = [1, 2, 3];
+    } else if ($_SESSION["offset"] >= $_SESSION["totalTuples"] - (2 * $_SESSION["limit"])) {
+        $valors = [ceil($_SESSION["totalTuples"] / $_SESSION["limit"]) - 2, ceil($_SESSION["totalTuples"] / $_SESSION["limit"]) - 1, ceil($_SESSION["totalTuples"] / $_SESSION["limit"])];
+    } else {
+        $valors = [$_SESSION["offset"] / $_SESSION["limit"] , $_SESSION["offset"] / $_SESSION["limit"] +1, $_SESSION["offset"] / $_SESSION["limit"] + 2];
+    }
+
+    echo "<li class=page-item><a class=page-link href=index.php?pg=1 aria-label='first page' style='vertical-align: middle'>&laquo;</a></li>";
+    echo "<li class=page-item><a class=page-link href=index.php?pg={$valors[0]} aria-label='first page' style='vertical-align: middle'>{$valors[0]}</a></li>";
+    echo "<li class=page-item><a class=page-link href=index.php?pg={$valors[1]} aria-label='first page' style='vertical-align: middle'>{$valors[1]}</a></li>";
+    echo "<li class=page-item><a class=page-link href=index.php?pg={$valors[2]} aria-label='first page' style='vertical-align: middle'>{$valors[2]}</a></li>";
+    echo "<li class=page-item><a class=page-link href=index.php?pg=darrera aria-label='first page' style='vertical-align: middle'>&raquo;</a></li>";
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="ca">
@@ -71,7 +112,6 @@ function imprimirFilesTabla($arrayFiles)
     thead {
         text-align: center;
     }
-
 
 </style>
 
@@ -96,13 +136,17 @@ function imprimirFilesTabla($arrayFiles)
             <th>Editorial<a href="index.php?sentit=asc&ordre=editor" class="btn <?php echo($_SESSION["selected"]=="editorasc")?"btn-primary":"btn-secundary"?>" style="margin-left: 5px">&#8593</a>
                 <a href="index.php?sentit=desc&ordre=editor" class="btn <?php echo($_SESSION["selected"]=="editordesc")?"btn-primary":"btn-secundary"?>">&#8595</a></th>
             <th style="vertical-align: middle">Acció</th>
-
         </tr>
         </thead>
         <tbody>
         <?php imprimirFilesTabla($_SESSION["arrayFiles"]) ?>
         </tbody>
     </table>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+<?php imprimirPaginacio()?>
+        </ul>
+    </nav>
 </div>
 </body>
 </html>
